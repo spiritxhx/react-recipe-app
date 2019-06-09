6bd9e8c6e6bc67c5456ef3c202041986f4b1a4fc
spiritxhx <spiritxhx@gmail.com>
2019-06-09 09:52:49 -0400

    initialize the App.js and some imports

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class RecipeDetail extends Component {
+export default class RecipeDetails extends Component {
   render() {
     return (
       <React.Fragment>
