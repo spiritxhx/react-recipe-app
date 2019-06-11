50f264c43d65af3eb9f3b6223ad4c6687ed82e7b
spiritxhx <spiritxhx@gmail.com>
2019-06-11 13:22:20 -0400

    add the button and details display on the Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,22 +3,34 @@ import React, { Component } from 'react';
 
 export default class Recipe extends Component {
   render() {
-    const{
+    const {
       image_url,
       title,
       source_url,
-      publisher, 
+      publisher,
       recipe_id
     } = this.props.recipe;
-    
+
     return (
       <React.Fragment>
         <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
           <div className="card">
-            <img src={image_url} 
-            className="img-card-top"
-            style={{height:"14rem"}}
-            alt="recipes"/>
+            <img src={image_url}
+              className="img-card-top"
+              style={{ height: "14rem" }}
+              alt="recipes" />
+            <div className="card-body text-capitalize">
+              <h6>{title}</h6>
+              <h6 className="text-warning text-slanted">
+                provided by {publisher}
+              </h6>
+            </div>
+            <div className="card-footer">
+              <button type="button"
+                className="btn btn-primary text-capitalize">
+                details
+                </button>
+            </div>
           </div>
         </div>
       </React.Fragment>
