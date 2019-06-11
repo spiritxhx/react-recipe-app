614a3fc0fe41dd0c8ea38ee9307f1fe9d4089310
spiritxhx <spiritxhx@gmail.com>
2019-06-11 12:57:58 -0400

    build the title of the recipe list

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import  Recipe from './Recipe';
+import Recipe from './Recipe';
 import RecipeSearch from './RecipeSearch';
 
 export default class RecipeList extends Component {
-  
+
   render() {
-    const {recipes} = this.props;
+    const { recipes } = this.props;
     return (
       <React.Fragment>
         <RecipeSearch />
-
-
+        <div className="container my-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* end of title */}
+        </div>
         <Recipe />
       </React.Fragment>
     )
