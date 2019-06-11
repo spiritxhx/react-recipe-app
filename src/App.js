0d2a33782829de372fd11a179efb60226141833c
spiritxhx <spiritxhx@gmail.com>
2019-06-10 23:50:48 -0400

    create the async await function

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ class App extends Component {
     recipes: [], 
     url: "https://www.food2fork.com/api/search?key=c3ddee385c6ce3caeb5ea9d600b07913"
   };
-  
+
+  async getRecipes(){
+    const data = await fetch(this.state.url);
+    const jsonData = await data.json();
+
+    
+  }
+
   render() {
     return (
       <React.Fragment>
