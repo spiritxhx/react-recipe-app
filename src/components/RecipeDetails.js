f62928c4b4785662fe5c2f36e84bf0ae9ef8e4cd
spiritxhx <spiritxhx@gmail.com>
2019-06-12 23:43:42 -0400

    build the constructor of RecipeDetails and add recipe and url to state

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import {recipe} from '../tempDetails';
+import { recipe } from '../tempDetails';
 
 export default class RecipeDetails extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      recipe:{}, 
+      url: `https://www.food2fork.com/api/get?key=c3ddee385c6ce3caeb5ea9d600b07913&rId=35382`
+    }
+  }
   render() {
     return (
       <React.Fragment>
-        <h1>Hello from Details</h1>       
+        <h1>Hello from Details</h1>
       </React.Fragment>
     )
   }
