f599490b1ad24ebab45e495a29a0705558824f2d
spiritxhx <spiritxhx@gmail.com>
2019-06-12 23:54:47 -0400

    use the hardcode recipe to avoid the 50 times limit

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { recipe } from '../tempDetails';
 
 export default class RecipeDetails extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      recipe:{}, 
-      url: `https://www.food2fork.com/api/get?key=c3ddee385c6ce3caeb5ea9d600b07913&rId=35382`
+      recipe: recipe,
+      url: `https://www.food2fork.com/api/get?key=c3ddee385c6ce3caeb5ea9d600b07913&rId=${this.props.id}`
     }
   }
   render() {
+    console.log(this.state.recipe);
+    
     return (
       <React.Fragment>
         <h1>Hello from Details</h1>
